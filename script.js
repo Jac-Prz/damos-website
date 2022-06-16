1b11bbb9722d4370929a8c287b0e562d6baffa3c
unknown <jac.prz@live.com>
2022-06-16 20:20:48 +0200

    contact and publication pages, fix links in navbar, made a footer

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ var links = linkContainer.getElementsByTagName('a');
 for ( i = 0 ; i < links.length ; i++){
   links[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += "active";
+
   });
 };
